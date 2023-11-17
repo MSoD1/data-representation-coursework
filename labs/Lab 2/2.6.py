fd04ed36e1776a819f09fdf2e65b4f7384a7bd0f
Megan <117634588+MSoD1@users.noreply.github.com>
2023-11-17 17:59:19 +0000

    Update 2.6.py removed newline

--- labs/Lab 2/2.6.py
+++ labs/Lab 2/2.6.py
@@ -3,7 +3,7 @@
 #you indent the for loop so that it is in the with block.
 
 #This fails to load the data into a csv since the url is an xml file
-# an empty csv is created
+# an empty csv is created  --- removing newline ='' got the code to work
 from xml.dom.minidom import parseString
 import requests
 import csv 
@@ -12,7 +12,7 @@ url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 page = requests.get(url)
 doc = parseString(page.content)
 
-with open('week03_train.csv', mode='w', newline='') as train_file:
+with open('week03_train.csv', mode='w') as train_file:
     train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             
     objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
